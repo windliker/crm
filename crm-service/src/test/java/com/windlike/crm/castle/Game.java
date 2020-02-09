d869a27c8dce22a2620dd711eb942bdaf381ef14
windlike <windlike@example.com>
2020-02-09 18:35:32 +0800

    代码优化2：成员变量私有化，通过封装来弱化类间关系即降耦合

--- crm-service/src/test/java/com/windlike/crm/castle/Game.java
+++ crm-service/src/test/java/com/windlike/crm/castle/Game.java
@@ -50,20 +50,8 @@ public class Game {
 
     private void goRoom(String direction) 
     {
-        Room nextRoom = null;
-        if(direction.equals("north")) {
-            nextRoom = currentRoom.northExit;
-        }
-        if(direction.equals("east")) {
-            nextRoom = currentRoom.eastExit;
-        }
-        if(direction.equals("south")) {
-            nextRoom = currentRoom.southExit;
-        }
-        if(direction.equals("west")) {
-            nextRoom = currentRoom.westExit;
-        }
-
+        // 降耦合2
+        Room nextRoom = currentRoom.getExit(direction);
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         }
@@ -76,14 +64,8 @@ public class Game {
     public void showPrompt() {
         System.out.println("你当前在" + currentRoom);
         System.out.print("出口有: ");
-        if(currentRoom.northExit != null)
-            System.out.print("north ");
-        if(currentRoom.eastExit != null)
-            System.out.print("east ");
-        if(currentRoom.southExit != null)
-            System.out.print("south ");
-        if(currentRoom.westExit != null)
-            System.out.print("west ");
+        // 降耦合1
+        System.out.print(currentRoom.getExitDesc());
         System.out.println();
     }
     
--- crm-service/src/test/java/com/windlike/crm/castle/Room.java
+++ crm-service/src/test/java/com/windlike/crm/castle/Room.java
@@ -1,11 +1,12 @@
 package com.windlike.crm.castle;
 
 public class Room {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    // 成员变量私有化
+    private String description;
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
 
     public Room(String description) 
     {
@@ -28,5 +29,41 @@ public class Room {
     public String toString()
     {
         return description;
+    }
+    
+    public String getExitDesc() {
+        StringBuffer sb = new StringBuffer();
+        
+        if (northExit != null) {
+            sb.append("north ");
+        }
+        if (southExit != null) {
+            sb.append("south ");
+        }
+        if (westExit != null) {
+            sb.append("west ");
+        }
+        if (eastExit != null) {
+            sb.append("east ");
+        }
+        
+        return sb.toString();
+    }
+    
+    public Room getExit(String direction) {
+        Room ret = null;
+        if(direction.equals("north")) {
+            ret = northExit;
+        }
+        if(direction.equals("east")) {
+            ret = eastExit;
+        }
+        if(direction.equals("south")) {
+            ret = southExit;
+        }
+        if(direction.equals("west")) {
+            ret = westExit;
+        }
+        return ret;
     }
 }
